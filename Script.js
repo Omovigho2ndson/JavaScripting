699322a0568b63f9113380905332c3dc88366b9b
Omovigho2ndson <erewhesa.o.francis@gmail.com>
2025-07-13 16:44:36 +0100

    Add files via upload

--- Script.js
+++ Script.js
@@ -1,45 +1,67 @@
-  function temperature()
+function temperature()
           {
              //To convert celsius to farenheit
              //(CEL * 9/5) + 32
-           var c = document.getElementById("celsius").value;
-           var f = (c * 9/5) + 32
-           document.getElementById("fahrenheit").value = f
+            var c = document.getElementById("celsius").value;
+            var f = (c * 9/5) + 32
+            document.getElementById("fahrenheit").value = f
 
+            if (c == ""){
+                alert("Enter value for Celsius");
+                c.focus;
+                return false;
+            }
+            return true
           }
-          function weight()
+
+function weight()
           
           {  //To convert KGs to Pounds
             // KG * 2.2
             var kg = document.getElementById("kilogram").value;
             var p = kg * 2.2
             document.getElementById("pounds").value = p
+
+            if (kg == ""){
+                alert("Please enter value for Kilogram");
+                kg.focus;
+                return false;
+            }
+            return true;
         }
-        function distance()
+
+function distance()
         {
             //To convert KMs to Miles
             // KM * 0.62137
             var km = document.getElementById("kilometer").value;
             var ml = km * 0.62137
             document.getElementById("miles").value = ml
+
+            if (km == ""){
+                alert("Please nter value for Kilometer");
+                km.focus;
+                return false;
+            }
+            return true;
         }
         
         // function to clear celsius and fahrenheit fields
-        function clearTemp()
+function clearTemp()
         {
             var cel = document.getElementById("celsius").value = ''
             var fah = document.getElementById("fahrenheit").value = ''
         }
           
         // function to clear kilogram and pounds field
-        function clearWeight()
+ function clearWeight()
         {
             var kg = document.getElementById("kilogram").value = ''
             var pd = document.getElementById("pounds").value = ''
         }
             
         // Function to clear kilometer and miles 
-        function clearDist()
+function clearDist()
         {
             var km = document.getElementById("kilometer").value = ''
             var mil = document.getElementById("miles").value = ''
